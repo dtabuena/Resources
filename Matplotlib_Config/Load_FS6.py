9743a0f099b67c19a8d0ef4cb9bd88a7bdf9a890
dtabuena <52722000+dtabuena@users.noreply.github.com>
2024-07-25 14:41:53 -0700

    Create Load_FS6.py

---
+++ Matplotlib_Config/Load_FS6.py
@@ -0,0 +1,21 @@
+import matplotlib as mpl
+from matplotlib import rcParams
+fig_config =    {
+    "font.size": 6,
+    "font.family": "arial",
+    "axes.linewidth": 0.5,
+    "xtick.major.width": 0.5,
+    "ytick.major.width": 0.5,
+    "figure.titlesize": 6,
+    "axes.titlesize": 6,
+    "xtick.labelsize": 6,
+    "ytick.labelsize": 6,
+    "axes.labelsize": 6,
+    "legend.fontsize": 6,
+    "savefig.dpi": 300,
+    "figure.dpi": 300,
+    "grid.color": "black",
+    "grid.linestyle": "-",
+    "grid.linewidth": 0.3
+    }
+rcParams.update(fig_config)
